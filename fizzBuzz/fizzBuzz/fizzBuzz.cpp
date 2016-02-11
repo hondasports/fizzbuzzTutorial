b2d2bbc902b678a0ee4611ca66046a54133cd73c
Tatsuya Miyamoto <hondasports@outlook.com>
2016-02-11 17:32:45 +0900

    initial commit. it is implemented plain fizzbuzz logic.

---
+++ fizzBuzz/fizzBuzz/fizzBuzz.cpp
@@ -0,0 +1,24 @@
+// fizzBuzz.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+int main()
+{
+	for (int i = 1; i < 50; i++) {
+
+		if (i % 3 == 0 && i % 5 == 0) {
+			std::cout << "fizzbuzz" << std::endl;
+		}
+		else if(i%3 == 0){
+			std::cout << "fizz" << std::endl;
+		}
+		else if (i % 5 == 0) {
+			std::cout << "buzz" << std::endl;
+		}
+		else {
+			std::cout << i << std::endl;
+		}
+	}
+    return 0;
+}
